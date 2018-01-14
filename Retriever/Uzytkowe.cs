71ce6c0b1298827a4129f2cbf13a42656c76a2be
Duch003 <tomaszmankin003@gmail.com>
2018-01-14 15:52:28 +0100

    .

--- Retriever/Uzytkowe.cs
+++ Retriever/Uzytkowe.cs
@@ -91,19 +91,19 @@ namespace Retriever
             //Jeżeli status aktywacji == 0 (wymagana aktywacja)
             if (Convert.ToInt32(myClass.Properties["ActivationRequired"].Value) == 0)
             {
-                #region Utworzenie łącznika z kartami sieciowymi i ustawienie połączenia na WindowsActivation
-                WlanClient Client = new WlanClient();
-                string profileName = "WindowsActivation";
-                string mac = "57696E646F777341637469766174696F6E";
-                string profileXml = string.Format("<?xml version=\"1.0\"?><WLANProfile xmlns=\"http://www.microsoft.com/networking/WLAN/profile/v1\"><name>{0}</name><SSIDConfig><SSID><hex>{1}</hex><name>{0}</name></SSID></SSIDConfig><connectionType>ESS</connectionType><connectionMode>manual</connectionMode><MSM><security><authEncryption><authentication>open</authentication><encryption>none</encryption><useOneX>false</useOneX></authEncryption></security></MSM></WLANProfile>", profileName, mac);
+                //#region Utworzenie łącznika z kartami sieciowymi i ustawienie połączenia na WindowsActivation
+                //WlanClient Client = new WlanClient();
+                //string profileName = "WindowsActivation";
+                //string mac = "57696E646F777341637469766174696F6E";
+                //string profileXml = string.Format("<?xml version=\"1.0\"?><WLANProfile xmlns=\"http://www.microsoft.com/networking/WLAN/profile/v1\"><name>{0}</name><SSIDConfig><SSID><hex>{1}</hex><name>{0}</name></SSID></SSIDConfig><connectionType>ESS</connectionType><connectionMode>manual</connectionMode><MSM><security><authEncryption><authentication>open</authentication><encryption>none</encryption><useOneX>false</useOneX></authEncryption></security></MSM></WLANProfile>", profileName, mac);
 
-                foreach (WlanClient.WlanInterface wlanIface in Client.Interfaces)
-                {
-                    wlanIface.SetProfile(Wlan.WlanProfileFlags.AllUser, profileXml, true);
-                    wlanIface.Connect(Wlan.WlanConnectionMode.Profile, Wlan.Dot11BssType.Any, profileName);
-                }
-                #endregion
-                myClass.InvokeMethod("ActivateOnline", null);
+                //foreach (WlanClient.WlanInterface wlanIface in Client.Interfaces)
+                //{
+                //    wlanIface.SetProfile(Wlan.WlanProfileFlags.AllUser, profileXml, true);
+                //    wlanIface.Connect(Wlan.WlanConnectionMode.Profile, Wlan.Dot11BssType.Any, profileName);
+                //}
+                //#endregion
+                //myClass.InvokeMethod("ActivateOnline", null);
             }
         }
     }
