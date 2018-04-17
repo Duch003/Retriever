52ba8c374390b8ff1f0bbdc2a511709d53ebd5db
Duch003 <tomaszmankin003@gmail.com>
2018-04-17 14:07:08 +0100

    Rozpoczęcie przebudowy Retrievera

---
+++ Retriever2.0/InfoBox.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Retriever2._0 {
+    /// <summary>
+    /// Interaction logic for InfoBox.xaml
+    /// </summary>
+    public partial class InfoBox : UserControl {
+        public InfoBox() {
+            InitializeComponent();
+        }
+    }
+}
